79afe3fa2d351331e76446b172ef4b7b1c66ae60
Nathan Touchton <99927853+NathanTouchton@users.noreply.github.com>
2022-06-17 18:34:07 -0500

    Add files via upload

--- Snake Game/scoreboard.py
+++ Snake Game/scoreboard.py
@@ -1,1 +1,20 @@
-# nothing for now
+from turtle import Turtle, Screen
+from food import Food
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+
+food = Food()
+
+display = Turtle()
+display.hideturtle()
+display.penup()
+display.color("white")
+display.speed(0)
+display.goto(0, 270)
+
+display.write("Test", align="center", font=("Arial", 10))
+
+
+screen.exitonclick()
